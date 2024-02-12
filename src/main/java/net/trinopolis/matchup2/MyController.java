9201c92122e1225251aa4d6b52618427f28a2ab7
Taku2002 <takudzwamusa73@gmail.com>
2024-02-12 09:04:30 +0200

    src

--- src/main/java/net/trinopolis/matchup2/MyController.java
+++ src/main/java/net/trinopolis/matchup2/MyController.java
@@ -1,14 +1,15 @@
 package net.trinopolis.matchup2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 public class MyController {
@@ -16,19 +17,21 @@ public class MyController {
     @Autowired
     private ImageManager imageManager;
 
-    @Autowired
-    private MediaSelector mediaSelector;
-
     @PostMapping("/upload/image")
     public ResponseEntity<String> uploadImage(@RequestBody MultipartFile file) {
         imageManager.uploadImage(file);
         return ResponseEntity.ok("Image uploaded successfully");
     }
 
+    @Autowired
+    private MediaSelector mediaSelector;
+
     @GetMapping("/random/media")
     public ResponseEntity<URI> getRandomMedia() {
-        URI mediaUri = mediaSelector.selectRandomMedia();
-        return ResponseEntity.ok(mediaUri);
+        // Get a random media URI
+        return Optional.ofNullable(mediaSelector.selectRandomMedia())
+                .map(ResponseEntity::ok) // Map to ResponseEntity.ok if URI is present
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)); // Otherwise, return 404
     }
 }
 
